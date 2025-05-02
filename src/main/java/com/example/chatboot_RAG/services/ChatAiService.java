b7f365349d3659ac8081cfe8a7f907a9cbfe9b1e
aichaoukdour <147880095+aichaoukdour@users.noreply.github.com>
2025-05-02 23:34:36 +0200

    first commit chat

--- src/main/java/com/example/chatboot_RAG/services/ChatAiService.java
+++ src/main/java/com/example/chatboot_RAG/services/ChatAiService.java
@@ -1,11 +1,12 @@
 package com.example.chatboot_RAG.services;
 
-import java.lang.module.ModuleDescriptor.Builder;
+
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatAiService {
+
 
    
 }
