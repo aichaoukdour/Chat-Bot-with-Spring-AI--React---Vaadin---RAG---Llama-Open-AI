13e3f2eedd27c715b993aad1d61ffb5322ae711b
aichaoukdour <147880095+aichaoukdour@users.noreply.github.com>
2025-05-04 23:43:08 +0200

    first estt chat

--- src/main/java/com/example/chatboot_RAG/services/ChatAiService.java
+++ src/main/java/com/example/chatboot_RAG/services/ChatAiService.java
@@ -2,10 +2,25 @@ package com.example.chatboot_RAG.services;
 
 
 
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClient.CallResponseSpec;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatAiService {
+    private ChatClient chatClient;
+
+    public ChatAiService(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    public String ragChat(String question) {
+        return chatClient.prompt() chatClientRequest
+                .user(Question)
+                .call() CallResponseSpec
+                .content();
+    }
 
 
    
---
+++ src/main/java/com/example/chatboot_RAG/web/ChatRestController.java
@@ -0,0 +1,26 @@
+package com.example.chatboot_RAG.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.chatboot_RAG.services.ChatAiService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/api/chat")
+public class ChatRestController {
+
+    private ChatAiService chatAiService;
+
+    public ChatRestController(ChatAiService chatAiService) {
+        this.chatAiService = chatAiService;
+    }
+
+    @GetMapping("/ask")
+    public String ask(@ String question) {
+        return new chatAiService.ragChat(question);
+    }
+    
+}
